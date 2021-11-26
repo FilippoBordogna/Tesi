28e5842369aa23143d7271dfdb1a6f60fcc30166
Bordogna Filippo <bordognapippo99@gmail.com>
2021-11-26 01:44:09 +0100

    Inserimento login, logout e signup

--- produzione_scientifica/gbliometrics/urls.py
+++ produzione_scientifica/gbliometrics/urls.py
@@ -1,23 +1,8 @@
-"""produzione_scientifica URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-# IMPORT
+# Import Librerie Esterne all'app
 from django.urls import path, include
 from django.views.generic.base import TemplateView # Vista generica TemplateView
 
+# Import Librerie Interne all'app
 from . import views # Viste
 from .views import SignUpView # Vista generica SignUpView
 
@@ -27,5 +12,5 @@ app_name = "gbliometrics" # Namespace per evitare futuri conflitti
 urlpatterns = [ # Url appartenenti all'app gbliometrics
     path('', TemplateView.as_view(template_name='home.html'), name = 'home'), # Pagina principale
     path('accounts/',  include('django.contrib.auth.urls')), # Autenticazione utente: riferimento alla libreria auth
-    path('accounts/signup/', SignUpView.as_view(), name='signup') # Registrazione utente: riferimento all view SignUpView
+    path('signup/', SignUpView.as_view(), name='signup') # Registrazione utente: riferimento all view SignUpView (views.py)
 ]
--- produzione_scientifica/gbliometrics/views.py
+++ produzione_scientifica/gbliometrics/views.py
@@ -1,12 +1,15 @@
+# Import Librerie Esterne all'app
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm # Creazione di form
 from django.urls import reverse_lazy
 from django.views import generic
+
+# Import Librerie Interne all'app
+from .forms import CustomUserCreationForm # Form di creazione utente personalizzata
 
 class SignUpView(generic.CreateView):
     '''
         Form di Registrazione all'applicazione
     '''
-    form_class = UserCreationForm # Tipo di form
+    form_class = CustomUserCreationForm # Tipo di form
     success_url = reverse_lazy('gbliometrics:login') # Url di redirezione in caso di successo ; reverse_lazy obbligatorio nelle viste generiche. 
     template_name = 'registration/signup.html' # Nome del template specifico che sovrascrive quello di default
