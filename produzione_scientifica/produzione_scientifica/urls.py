fcdda3482f01a757549a769d3793908b6441356a
Bordogna Filippo <bordognapippo99@gmail.com>
2021-12-17 06:05:51 +0100

    Modifica degli URL

--- produzione_scientifica/produzione_scientifica/urls.py
+++ produzione_scientifica/produzione_scientifica/urls.py
@@ -7,9 +7,9 @@ from django_registration.backends.activation.views import RegistrationView
 from gbliometrics.forms import CustomRegistrationForm
 
 urlpatterns = [
-    path('', include('frontend.urls')),
+    path('', include('frontend.urls')), # Inclusione delle url presenti nel file frontend/urls.py (frontend)
     path('admin/', admin.site.urls, name="admin"), # Pagina di amministrazione
-    path('api/', include('gbliometrics.urls')), # Inclusione delle url nel file gbliometrics/urls.py (backend)
+    path('api/', include('gbliometrics.urls')), # Inclusione delle url presenti nel file gbliometrics/urls.py (backend: API)
     path('accounts/',  include('django.contrib.auth.urls')), # Autenticazione utente: riferimento alla libreria auth
     path('accounts/register/', RegistrationView.as_view(form_class=CustomRegistrationForm), name='registration_register'), # Override della registrazione della libreria django_registration
     path('accounts/', include('django_registration.backends.activation.urls')), # Registrazione utente: riferimento alla libreria django_registration
