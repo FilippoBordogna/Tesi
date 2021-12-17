ac246e077411f5925ad9ac2a5bab3621301a03f5
Bordogna Filippo <bordognapippo99@gmail.com>
2021-12-17 06:05:28 +0100

    Creazione del frontend per creare un gruppo e vedere la lista dei gruppi

--- produzione_scientifica/frontend/urls.py
+++ produzione_scientifica/frontend/urls.py
@@ -4,5 +4,7 @@ from django.views.generic.base import TemplateView # Vista generica TemplateView
 app_name = "frontend" # Namespace per evitare futuri conflitti
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name = 'home'), # Pagina principale
+    path('hold/', TemplateView.as_view(template_name='hold.html'), name = 'old'), # Placeholder
+    path('', TemplateView.as_view(template_name='home.html'), name="home"), # Pagina principale
+    path('group', TemplateView.as_view(template_name='groups.html'), name='group-list')
 ]
