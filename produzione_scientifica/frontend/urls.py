6ac1e98c3957194d50017116c818ed684d85a964
Bordogna Filippo <bordognapippo99@gmail.com>
2021-12-20 19:22:27 +0100

    Modifica di 1 alias

--- produzione_scientifica/frontend/urls.py
+++ produzione_scientifica/frontend/urls.py
@@ -6,5 +6,5 @@ app_name = "frontend" # Namespace per evitare futuri conflitti
 urlpatterns = [
     path('hold/', TemplateView.as_view(template_name='hold.html'), name = 'old'), # Placeholder
     path('', TemplateView.as_view(template_name='home.html'), name="home"), # Pagina principale
-    path('group', TemplateView.as_view(template_name='groups.html'), name='group-list')
+    path('groups', TemplateView.as_view(template_name='groups.html'), name='group-list')
 ]
