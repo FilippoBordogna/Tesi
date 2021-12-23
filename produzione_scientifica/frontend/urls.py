a6f80f8fde4534624a5f1e3be7c62b86474dab6c
Bordogna Filippo <bordognapippo99@gmail.com>
2021-12-23 04:40:26 +0100

    Aggiunta la URL della pagina di dettaglio degli autori

--- produzione_scientifica/frontend/urls.py
+++ produzione_scientifica/frontend/urls.py
@@ -4,7 +4,8 @@ from django.views.generic.base import TemplateView # Vista generica TemplateView
 app_name = "frontend" # Namespace per evitare futuri conflitti
 
 urlpatterns = [
-    path('hold/', TemplateView.as_view(template_name='hold.html'), name = 'old'), # Placeholder
     path('', TemplateView.as_view(template_name='home.html'), name="home"), # Pagina principale
-    path('groups', TemplateView.as_view(template_name='groups.html'), name='group-list')
+    path('groups/', TemplateView.as_view(template_name='groups.html'), name='group-list'), # Pagina di gestione dei gruppi
+    path('groups/authors/<str:groupId>/', TemplateView.as_view(template_name='authors.html'), name='author-list'), # Pagina di gestione degli autori
+    #path('author-detail/<str:auhtorScopusId>/', TemplateView.as_view(template_name='author-detail.html'), name='author-detail')
 ]
