771cbc21f42f9b53b31a8e6d9936fecd018f26bf
Bordogna Filippo <bordognapippo99@gmail.com>
2021-12-24 01:39:19 +0100

    Aggiunta del link che porta alla pagina dei dettagli di un autore

--- produzione_scientifica/frontend/urls.py
+++ produzione_scientifica/frontend/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='home.html'), name="home"), # Pagina principale
     path('groups/', TemplateView.as_view(template_name='groups.html'), name='group-list'), # Pagina di gestione dei gruppi
     path('groups/authors/<str:groupId>/', TemplateView.as_view(template_name='authors.html'), name='author-list'), # Pagina di gestione degli autori
-    #path('author-detail/<str:auhtorScopusId>/', TemplateView.as_view(template_name='author-detail.html'), name='author-detail')
+    path('author-detail/<str:auhtorScopusId>/', TemplateView.as_view(template_name='author-detail.html'), name='author-detail')
 ]
